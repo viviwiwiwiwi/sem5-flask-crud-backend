88fdfc5380e4ed80660bdce20468ee3035d091fe
Vivian Ariana Cuadros Aira <variana2105@gmail.com>
2023-09-15 21:03:53 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,13 @@
+from dotenv  import load_dotenv
+import os
+
+
+load_dotenv()
+user=os.environ['USER']
+pwd=os.environ['PASSWORD']
+host=os.environ['HOST']
+database=os.environ['DATABASE']
+server=os.environ['SERVER']
+
+DATABASE_CONNECTION = f'{server}://{user}:{pwd}@{host}/{database}'
+
